6decbf5d108af6bd0cdc207e71804a301bbfe04f
Aditi gupta <aditigupta.ag226@email.com>
2025-07-06 18:25:45 +0530

    temperature convert

--- fahrenheittocelsius.java
+++ fahrenheittocelsius.java
@@ -1,4 +1,14 @@
 import java.util.Scanner;
 public class fahrenheittocelsius {
-    
+    public static void main(String[] args) {
+        float temperature;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter tempertaure in fahraenheit: ");
+        temperature = in.nextFloat();
+
+        temperature = ((temperature - 32)*5)/9;
+        System.out.println("temperature in celsius:" +temperature);
+
+    }
 }
