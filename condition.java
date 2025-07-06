5be7dae09cad4bc0e145103a4b7b5c4ea74ded2a
Aditi gupta <aditigupta.ag226@email.com>
2025-07-06 19:37:46 +0530

    condition

---
+++ condition.java
@@ -0,0 +1,12 @@
+public class condition {
+    public static void main(String[] args) {
+        boolean learning = false;
+
+        if (learning) {
+            System.out.println("java programmer");
+    }
+    else{
+        System.out.println("what are u doing here.....??");
+    }
+    }
+}
