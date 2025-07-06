46bba645b3eac33709131086edd5e4efd8c7d743
Aditi gupta <aditigupta.ag226@email.com>
2025-07-06 20:42:51 +0530

    star console in rows

---
+++ starconsole.java
@@ -0,0 +1,12 @@
+public class starconsole {
+    public static void main(String[] args) {
+        int row, numberofstars;
+
+        for(row =1;row <= 10;row++) {
+          for(numberofstars = 1; numberofstars <= row;numberofstars++) {
+            System.out.println("*");
+          }
+          System.out.println();
+    }    
+}
+}
