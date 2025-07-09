fd55cd506cc306b1dda6cc9abacb25de8d9d54fb
aditigupta0710 <aditigupta.ag226@gmail.com>
2025-07-09 21:16:26 +0530

    Update numericalrectangularpattern.java

--- numericalrectangularpattern.java
+++ numericalrectangularpattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class patterntriangle {
+public class numericalrectangularpattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int r = sc.nextInt();
