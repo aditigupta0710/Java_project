bb23352fc5c87bbbc7061dcb18e93c6b5ebbdd1c
Aditi gupta <aditigupta.ag226@email.com>
2025-07-18 20:27:46 +0530

    to reverse an array

---
+++ reversearray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class reversearray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int n = arr.length;
+        for (int i = 0;i < n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
