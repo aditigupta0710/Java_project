da1d9a87e0cfbacaf632facd4d5ae4914b020ac7
Aditi gupta <aditigupta.ag226@email.com>
2025-07-18 20:34:35 +0530

    to sort an array

---
+++ sortArray.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String[] args) {
+        int[] arr = {8,9,6,4,6,3,2};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    } 
+}
