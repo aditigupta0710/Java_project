4f88dd066b36cd929969082848c0b16030ba6d2c
Aditi gupta <aditigupta.ag226@email.com>
2025-07-19 22:09:04 +0530

    make twice using pass by value

---
+++ maketwice.java
@@ -0,0 +1,11 @@
+public class maketwice {
+    static void maketwice (int p) {
+        p *= 2;
+        System.out.println(p);
+    }
+    public static void main(String[] args) {
+     int p = 24;
+     maketwice(p);
+     System.out.println(p);
+    }
+}
