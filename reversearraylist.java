754ddc3166e193e490fe8b2d356bc08dc3d5573c
Aditi gupta <aditigupta.ag226@email.com>
2025-07-30 22:20:42 +0530

    reverse the arraylist

---
+++ reversearraylist.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+    public class reversearraylist {
+
+        static void reverselist(ArrayList<Integer> list) {
+            int i = 0, j = list.size()-1;
+            while(i<j){
+                /*
+                 * int temp = a;
+                 * a = b;
+                 * b = temp;
+                 */
+
+                 Integer temp = Integer.valueOf(list.get(i));
+                 list.set(i, list.get(j));
+                 list.set(j, temp);
+
+                 i++;
+                 j--;
+                
+            }
+        }
+        
+        public static void main(String[] args) {
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(0);
+            list.add(3);
+            list.add(5);
+            list.add(6);
+            list.add(7);
+            list.add(9);
+
+            System.out.println("original arraylist : " + list);
+            reverselist(list);
+
+            System.out.println("reversed list : " + list);
+            
+        }
+}
