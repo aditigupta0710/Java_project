0e4237cf8d24772a8efd17d89138a38a0028fd7e
Aditi gupta <aditigupta.ag226@email.com>
2025-07-30 22:02:22 +0530

    index occuring of arraylist

--- addelements.java
+++ addelements.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
     public class addelements {
     public static void main(String[] args) {
-        ArrayList <Object> l = new ArrayList<>();
+        ArrayList l = new ArrayList();
         l.add(23);
         l.add("Aditi");
         l.add(true);
 
-        System.out.println(l);
+        System.out.println();
     }
 }
