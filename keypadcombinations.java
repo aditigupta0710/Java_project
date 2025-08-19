0f4733afea91497094aacb541dc570d7c668d7e2
Aditi gupta <aditigupta.ag226@gmail.com>
2025-08-19 23:09:58 +0530

    keypad combinations

---
+++ keypadcombinations.java
@@ -0,0 +1,18 @@
+public class keypadcombinations {
+    static void combination(String dig, String[] kp, String result) { //253
+    if(dig.length() == 0) {
+        System.out.println(result + " ");
+        return;
+    }    
+    int currNum = dig.charAt(0) - '0'; //2
+    String currChoices = kp[currNum]; //abc
+    for(int i = 0; i < currChoices.length(); i++) { //"abc"
+    combination(dig.substring(1), kp, result+currChoices.charAt(i));
+       }
+    }
+    public static void main(String[] args) {
+        String dig = "23";
+        String[] kp = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz",};
+        combination(dig, kp, " ");
+    }
+}
