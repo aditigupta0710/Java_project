029cb3b2f59e872556f4b736d788b9b89781cb17
Aditi gupta <aditigupta.ag226@gmail.com>
2025-09-13 22:34:59 +0530

    delete indexelement  of  linked list

---
+++ deleteindex.java
@@ -0,0 +1,86 @@
+public class deleteindex {
+    public static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    public static class linkedlist {
+        Node head = null;
+        Node tail = null;
+        
+        void InsertAtEnd (int data) {
+            Node temp = new Node(data);
+            if(head == null) {
+                head = temp;
+            } else{
+                tail.next = temp;
+            } 
+            tail = temp;
+        }
+
+        void InsertAtBeginning (int data){
+            Node temp = new Node(data);
+            if(head == null) {
+                head = tail =  temp;
+            } else {
+            temp.next = head;
+            head = temp;
+            }
+        }
+        void InsertAt(int idx, int data) {
+            Node t = new Node(data);
+            Node temp = head;
+            for(int i = 0; i <= idx-1;i++) {
+                temp = temp.next;
+            }
+            t.next = temp.next;
+            temp.next = t;
+        }
+
+        int getat(int idx) {
+            Node temp = head;
+            for(int i = 1; i <= idx; i++) {
+                temp = temp.next;
+            }
+            return temp.data;
+        }
+
+        void deleteAt(int idx) {
+            Node temp = head;
+            for(int i = 1; i <= idx; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
+        void display() {
+            Node temp  = head;
+            while(temp != null) {
+                System.out.println(temp.data + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        linkedlist ll = new linkedlist();
+        ll.InsertAtEnd(89);
+        ll.display();
+        ll.InsertAtEnd(83);
+        ll.display();
+        ll.InsertAtEnd(8);
+        ll.display();
+        ll.InsertAtEnd(7);
+        ll.display();
+        ll.InsertAtBeginning(28);
+        ll.display();
+        ll.InsertAt(3, 56);
+        ll.display();
+        ll.InsertAt(2, 46);
+        ll.display();
+        System.out.println(ll.getat(2));
+        ll.deleteAt(2);
+        ll.display();
+    }
+}
