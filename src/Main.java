78799759a9c1cf6bd2427da8ac1af03b0b3d3471
Aditi gupta <aditigupta.ag226@gmail.com>
2025-12-30 21:42:54 +0530

    Added Intellij Java code

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        String name = "Aditi";
+//        System.out.println(name.charAt(1));
+        //replace
+//        String name2 = name.replace('i','c');
+//        System.out.println(name2);
+        //substring
+        String name = "Aditi and prince";
+        System.out.println(name.substring(0,6));
+    }
+}
